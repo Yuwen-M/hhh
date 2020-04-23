6340b2470c848582cc302e7e2f97fdb5a23a0329
Yuwen-M <1296989938@qq.com>
2020-04-23 14:35:06 +0800

    111

---
+++ src/test/my.java
@@ -0,0 +1,13 @@
+package test;
+
+/**
+ * @Author: mqw
+ * @Date: 2020/4/23 14:32
+ */
+public class my {
+    public static void main(String[] args) {
+        System.out.println(
+                123
+        );
+    }
+}
